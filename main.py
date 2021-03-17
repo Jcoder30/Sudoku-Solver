530ba81198f21460863eb4115dd0ddd6c2ba513b
Jonah <77639078+Jonah-30@users.noreply.github.com>
2021-03-17 17:13:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,71 +1,5 @@
-import math
 import numpy as np
-import copy
-
-
-# visually formats the board
-def format_board(board, l):
-    base = math.floor(math.sqrt(l))
-    side = l
-
-    def expand_line(line, base):
-        return line[0] + line[5:9].join([line[1:5] * (base - 1)] * base) + line[9:13]
-
-    line0 = expand_line("╔═══╤═══╦═══╗", base)
-    line1 = expand_line("║ . │ . ║ . ║", base)
-    line2 = expand_line("╟───┼───╫───╢", base)
-    line3 = expand_line("╠═══╪═══╬═══╣", base)
-    line4 = expand_line("╚═══╧═══╩═══╝", base)
-
-    symbol = " 1234567890"  # ABCDEFGHIJKLMNOPQRSTUVWXYZ
-    nums = [[""] + [symbol[n] for n in row] for row in board]
-    print(line0)
-    for r in range(1, side + 1):
-        print("".join(n + s for n, s in zip(nums[r - 1], line1.split("."))))
-        print([line2, line3, line4][(r % side == 0) + (r % base == 0)])
-
-
-# Checks if a spot can be played using a certain number
-def check_spot(board, x, y, n, l):
-    for i in range(0, l):
-        if board[x][i] == n or board[i][y] == n:
-            return False
-    sqrt_l = math.floor(math.sqrt(l))
-    x0 = (x // sqrt_l) * sqrt_l
-    y0 = (y // sqrt_l) * sqrt_l
-    for i in range(0, sqrt_l):
-        for j in range(0, sqrt_l):
-            if board[x0 + i][y0 + j] == n:
-                return False
-    return True
-
-
-# checks if board is completely filled
-def board_complete(board, l):
-    complete = True
-    for x in range(0, l):
-        for y in range(0, l):
-            if board[x][y] == 0:
-                complete = False
-                return complete
-    return complete
-
-
-# Solves a sudoku board
-def solve(board, l):
-    answer = []
-    for x in range(0, l):
-        for y in range(0, l):
-            if board[x][y] == 0:
-                for n in range(1, l + 1):
-                    if check_spot(board, x, y, n, l):
-                        board[x][y] = n
-                        board = solve(board, l)
-                        if board_complete(board, l):
-                            answer = copy.deepcopy(board)
-                        else:
-                            board[x][y] = 0
-            return board
+import math
 
 
 # Generates a random sudoku board
@@ -95,13 +29,60 @@ def random_board(l):
     empties = squares * 3 // 4
     for p in sample(range(squares), empties):
         board[p // side][p % side] = 0
-    # board = format_board(board, l)
+
     return board
 
 
+def possible(board, x, y, n, l):
+    # Is the number appearing in the given row?
+    for i in range(0, l):
+        if board[x][i] == n or board[i][y] == n:
+            return False
+
+    # Is the number appearing in the given square?
+    sqrt_l = math.floor(math.sqrt(l))
+    x0 = (y // sqrt_l) * sqrt_l
+    y0 = (x // sqrt_l) * sqrt_l
+    for i in range(0, sqrt_l):
+        for j in range(0, sqrt_l):
+            if board[y0 + i][x0 + j] == n:
+                return False
+
+    return True
+
+
+def solve(board, l):
+    board_number = 1
+    for x in range(0, l):
+        for y in range(0, l):
+            if board[x][y] == 0:
+                for n in range(1, l + 1):
+                    if possible(board, x, y, n, l):
+                        board[x][y] = n
+                        solve(board, l)
+                        board[x][y] = 0
+
+                return
+
+    print('New Solution' + '\n', np.matrix(board))
+    board_number += 1
+    input('Next possible solution?')
+
+
+# test 9 by 9 board with only one solution
+test_board = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
+              [6, 0, 0, 1, 9, 5, 0, 0, 0],
+              [0, 9, 8, 0, 0, 0, 0, 6, 0],
+              [8, 0, 0, 0, 6, 0, 0, 0, 3],
+              [4, 0, 0, 8, 0, 3, 0, 0, 1],
+              [7, 0, 0, 0, 2, 0, 0, 0, 6],
+              [0, 6, 0, 0, 0, 0, 2, 8, 0],
+              [0, 0, 0, 0, 1, 9, 0, 0, 5],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+# Length must be a perfect square
 length = 9
+# creates random board depending on the length (the board may have more than one solution)
 play_board = random_board(length)
-print(np.matrix(play_board))
-print(np.matrix(solve(play_board, length)))
-# solve(length)
-# print(np.matrix(play_board))
+print('Starting Board\n', np.matrix(play_board))
+solve(play_board, length)
